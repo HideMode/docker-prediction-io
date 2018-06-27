eb1da687088d0afde59d735747fbe91d7a328f7c
Manito <mantao@acxiom.com>
2018-06-27 17:06:53 +0800

    hadoop集群

---
+++ prediction/volumes/pio-env.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+# SAMPLE OF pio-env.sh file ####################################################
+
+# Copy this file as pio-env.sh and edit it for your site's configuration.
+
+# PredictionIO Main Configuration
+#
+# This section controls core behavior of PredictionIO. It is very likely that
+# you need to change these to fit your site.
+
+# SPARK_HOME: Apache Spark is a hard dependency and must be configured.
+SPARK_HOME=$PIO_HOME/vendors/spark
+
+# ES_CONF_DIR: You must configure this if you have advanced configuration for
+#              your Elasticsearch setup.
+# ES_CONF_DIR=/opt/elasticsearch
+
+# HADOOP_CONF_DIR: You must configure this if you intend to run PredictionIO
+#                  with Hadoop 2.
+# HADOOP_CONF_DIR=/opt/hadoop
+
+# HBASE_CONF_DIR: You must configure this if you intend to run PredictionIO
+#                 with HBase on a remote cluster.
+# HBASE_CONF_DIR=$PIO_HOME/vendors/hbase-1.0.0/conf
+
+# Filesystem paths where PredictionIO uses as block storage.
+PIO_FS_BASEDIR=$HOME/.pio_store
+PIO_FS_ENGINESDIR=$PIO_FS_BASEDIR/engines
+PIO_FS_TMPDIR=$PIO_FS_BASEDIR/tmp
+
+# PredictionIO Storage Configuration
+#
+# This section controls programs that make use of PredictionIO's built-in
+# storage facilities. Default values are shown below.
+#
+# For more information on storage configuration please refer to
+# https://docs.prediction.io/system/anotherdatastore/
+
+# Storage Repositories
+
+# Default is to use PostgreSQL
+#PIO_STORAGE_REPOSITORIES_METADATA_NAME=pio_meta
+#PIO_STORAGE_REPOSITORIES_METADATA_SOURCE=PGSQL
+#
+#PIO_STORAGE_REPOSITORIES_EVENTDATA_NAME=pio_event
+#PIO_STORAGE_REPOSITORIES_EVENTDATA_SOURCE=PGSQL
+#
+#PIO_STORAGE_REPOSITORIES_MODELDATA_NAME=pio_model
+#PIO_STORAGE_REPOSITORIES_MODELDATA_SOURCE=PGSQL
+
+# Default is to use PostgreSQL but for clustered scalable setup we'll use
+# Elasticsearch
+PIO_STORAGE_REPOSITORIES_METADATA_NAME=pio_meta
+PIO_STORAGE_REPOSITORIES_METADATA_SOURCE=ELASTICSEARCH
+
+PIO_STORAGE_REPOSITORIES_EVENTDATA_NAME=pio_event
+PIO_STORAGE_REPOSITORIES_EVENTDATA_SOURCE=HBASE
+
+PIO_STORAGE_REPOSITORIES_MODELDATA_NAME=pio_model
+PIO_STORAGE_REPOSITORIES_MODELDATA_SOURCE=ELASTICSEARCH
+
+# Storage Data Sources
+
+# PostgreSQL Default Settings
+# Please change "pio" to your database name in PIO_STORAGE_SOURCES_PGSQL_URL
+# Please change PIO_STORAGE_SOURCES_PGSQL_USERNAME and
+# PIO_STORAGE_SOURCES_PGSQL_PASSWORD accordingly
+#PIO_STORAGE_SOURCES_PGSQL_TYPE=jdbc
+#PIO_STORAGE_SOURCES_PGSQL_URL=jdbc:postgresql://localhost/pio
+#PIO_STORAGE_SOURCES_PGSQL_USERNAME=pio
+#PIO_STORAGE_SOURCES_PGSQL_PASSWORD=pio
+
+# MySQL Example
+# PIO_STORAGE_SOURCES_MYSQL_TYPE=jdbc
+# PIO_STORAGE_SOURCES_MYSQL_URL=jdbc:mysql://localhost/pio
+# PIO_STORAGE_SOURCES_MYSQL_USERNAME=pio
+# PIO_STORAGE_SOURCES_MYSQL_PASSWORD=pio
+
+# Elasticsearch Example
+PIO_STORAGE_SOURCES_ELASTICSEARCH_TYPE=elasticsearch
+PIO_STORAGE_SOURCES_ELASTICSEARCH_CLUSTERNAME=de-elasticsearch
+PIO_STORAGE_SOURCES_ELASTICSEARCH_HOSTS=de-elasticsearch
+PIO_STORAGE_SOURCES_ELASTICSEARCH_PORTS=9300
+PIO_STORAGE_SOURCES_ELASTICSEARCH_HOME=$PIO_HOME/vendors/elasticsearch
+
+# Local File System Example
+# PIO_STORAGE_SOURCES_LOCALFS_TYPE=localfs
+# PIO_STORAGE_SOURCES_LOCALFS_PATH=$PIO_FS_BASEDIR/models
+
+# HBase Example
+PIO_STORAGE_SOURCES_HBASE_TYPE=hbase
+PIO_STORAGE_SOURCES_HBASE_HOME=$PIO_HOME/vendors/hbase
+HBASE_CONF_DIR=$PIO_HOME/vendors/hbase/conf
